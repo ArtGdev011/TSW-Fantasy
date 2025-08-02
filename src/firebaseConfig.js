1c6d1f5d27df71c59aa4b51dbb569dbd2b9024d3
Ferat03 <fgashi@myriadsystems.ai>
2025-08-02 20:21:45 +0200

    Update Firebase configuration and fix hosting setup

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,28 +1,19 @@
-// src/firebaseConfig.js
-
-// Import the necessary Firebase functions from the SDKs
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBkpza15mv5CswxyrknUX7hIUKmTLuX0D0",
   authDomain: "tsw-fantasy.firebaseapp.com",
   projectId: "tsw-fantasy",
-  storageBucket: "tsw-fantasy.firebasestorage.app",
+  storageBucket: "tsw-fantasy.appspot.com",
   messagingSenderId: "380158093213",
   appId: "1:380158093213:web:1f2b2651cc601e56faf174"
 };
 
-// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
 
-// Initialize and export Firestore database instance
+export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-// Initialize and export Firebase Authentication instance
-export const auth = getAuth(app);
-
-// Optionally export the initialized app instance
-export default app;
+  
