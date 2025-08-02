a2d6d076d428874c183057421cd43ff9af008765
Ferat03 <fgashi@myriadsystems.ai>
2025-08-02 20:14:05 +0200

    Update Firebase configuration and add Firestore rules and indexes

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,11 @@
 // src/firebaseConfig.js
 
-// Import the necessary Firebase functions from SDKs
+// Import the necessary Firebase functions from the SDKs
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Your Firebase project configuration object
+// Your Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBkpza15mv5CswxyrknUX7hIUKmTLuX0D0",
   authDomain: "tsw-fantasy.firebaseapp.com",
@@ -15,7 +15,7 @@ const firebaseConfig = {
   appId: "1:380158093213:web:1f2b2651cc601e56faf174"
 };
 
-// Initialize Firebase app instance
+// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
 
 // Initialize and export Firestore database instance
@@ -24,5 +24,5 @@ export const db = getFirestore(app);
 // Initialize and export Firebase Authentication instance
 export const auth = getAuth(app);
 
-// Optionally export the initialized app (if needed elsewhere)
+// Optionally export the initialized app instance
 export default app;
